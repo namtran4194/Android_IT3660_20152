9b9181d35a4aa049dfe98401f5d186dac5fe3fd8
Nam <nam3vj@gmail.com>
2016-01-23 15:47:52 +0700

    add project

---
+++ SimpleForm/app/src/main/java/hust/namtran/simpleform/MainActivity.java
@@ -0,0 +1,31 @@
+package hust.namtran.simpleform;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    Restaurant r=new Restaurant();
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super. onCreate(savedInstanceState);
+        setContentView(R. layout. content_main);
+        Button save=(Button) findViewById(R. id. save);
+        save. setOnClickListener(onSave);
+    }
+    private View. OnClickListener onSave=new View. OnClickListener() {
+        public void onClick(View v) {
+            EditText name=(EditText) findViewById(R. id. name);
+            EditText address=(EditText) findViewById(R. id. addr);
+            r. setName(name.getText().toString());
+            r. setAddress(address.getText().toString());
+            Toast toast = Toast.makeText(MainActivity.this, "Saved",Toast.LENGTH_SHORT);
+            toast.show();
+            name.setText("");
+            address.setText("");
+        }
+    };
+}
---
+++ SimpleForm/app/src/main/java/hust/namtran/simpleform/Restaurant.java
@@ -0,0 +1,25 @@
+package hust.namtran.simpleform;
+
+/**
+ * Created by namtran on 22/01/2016.
+ */
+public class Restaurant {
+    private String name="";
+    private String address="";
+
+    public String getName() {
+        return(name);
+    }
+
+    public void setName(String name) {
+        this. name=name;
+    }
+
+    public String getAddress() {
+        return(address);
+    }
+
+    public void setAddress(String address) {
+        this. address=address;
+    }
+}
